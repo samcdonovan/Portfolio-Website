c562f8c4cbeb95f030fe2c30391b4eaed90b194b
Sam.Donovan <samcdonovan@gmail.com>
2022-08-22 16:53:12 +0100

    Updating About page

--- src/components/About.vue
+++ src/components/About.vue
@@ -13,30 +13,37 @@
   </div>
 
   <div class="about-body">
-    <h2>Interests</h2>
-    <p>
-      A Computer Science topic/field that I am deeply interested in is
-      Artificial Intelligence, being one of my favourite modules that I
-      undertook during my degree. Of the pieces of work that I produced for this
-      module, I found the Machine Learning Categoriser for the UCI digits task
-      the most interesting, as it allowed me to experiment with different ML
-      techniques (Nearest Neighbour, Genetic Algorithms, Neural networks,
-      Support Vector Machines) to varying degrees of success.
-    </p>
+    <div id="interests">
+      <div id="interests-body">
+        <h2>Interests</h2>
+        <p>
+          A Computer Science topic/field that I am deeply interested in is
+          Artificial Intelligence, being one of my favourite modules that I
+          undertook during my degree. Of the pieces of work that I produced for
+          this module, I found the Machine Learning Categoriser for the UCI
+          digits task the most interesting, as it allowed me to experiment with
+          different ML techniques (Nearest Neighbour, Genetic Algorithms, Neural
+          networks, Support Vector Machines) to varying degrees of success.
+        </p>
 
-    <p>
-      Another key module for me was Advanced Web Development and Big Data. This
-      module covered a range of key web development topics, techniques and
-      libraries gained an understanding of key libraries that are widely used in
-      the industry (e.g. Java’s Maven, TypeScript and Vue e.t.c)
-    </p>
+        <p>
+          Another key module for me was Advanced Web Development and Big Data.
+          This module covered a range of key web development topics, techniques
+          and libraries gained an understanding of key libraries that are widely
+          used in the industry (e.g. Java’s Maven, TypeScript and Vue e.t.c)
+        </p>
 
-    <p>
-      To see more details about the projects mentioned above (as well as other
-      projects), please visit the
-      <router-link id="projects-link" to="/projects">Projects page</router-link>
-      of this website.
-    </p>
+        <p>
+          To see more details about the projects mentioned above (as well as
+          other projects), please visit the
+          <router-link id="projects-link" to="/projects"
+            >Projects page</router-link
+          >
+          of this website.
+        </p>
+      </div>
+      <img src="../assets/interests-pic.jpg" />
+    </div>
     <h2>Hobbies And Interests</h2>
     <p>
       I find great joy in being outside, I have a keen interest in cycling and
@@ -45,31 +52,6 @@
       the Earth by Frantz Fanon, and I am currently reading “Songs of a dead
       dreamer” by Thomas Ligotti.
     </p>
-
-    Upon leaving college, I found myself not really knowing what it was that I
-    wanted to do with my life, and I also did not feel very confident in my
-    abilities. I applied for countless retail jobs, and after many interviews, I
-    eventually got accepted for a job at Argos, working as a Customer Assistant
-    and eventually moved on to be a Delivery Assistant. It was at this job where
-    I not only regained a lot of confidence, but also learnt a lot of the key
-    skills that I use at University, and plan on using at future jobs. As a
-    Customer Assistant, I learnt how to effectively meet targets in limited
-    time, interact with customers in a meaningful and friendly manner, and
-    manage stressful situations when required. It was these qualities that
-    helped me attain the 'Customer Hero' award for exceptional customer service.
-    As a Delivery Assistant, I had to work in a team to pull in and stock
-    deliveries timely and efficiently (few errors, well-placed items), and
-    eventually gained the responsibility of 'key holder', responsible for
-    opening/closing the store when necessary. Upon gaining this responsibility,
-    I also gained the responsibility of partially managing the delivery team,
-    handling stock control and dealing with issues that arose during delivery.
-    These traits were easily transferrable to my job as a Student Learning
-    Assistant during the Computer Science course. Team working was necessary to
-    manage large classes and deliver content in a helpful way for the students.
-    The initiative and responsibility that I gained from being a Delivery
-    Assistant helped when I decided to run weekly drop-in sessions for the
-    students, where I had to deliver my own planned out, structured sessions to
-    5 - 10 students by myself for an hour every week.
   </div>
 </template>
 
@@ -100,5 +82,20 @@ export default {
 }
 .about-body p {
   text-align: justify;
+}
+#interests {
+  float: left;
+  width: 100%;
+  height:80%;
+
+  position:relative;
+}
+#interests-body{
+  float:left;
+  width:55%;
+}
+#interests img {
+  width: 30%;
+  height:50%;
 }
 </style>
